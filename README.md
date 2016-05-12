27a7e452716dae1a0e420e2f729b1b3c1bb53084
LinuxArchitects <heddar.abdelilah@live.fr>
2016-05-12 23:50:04 +0200

    fist commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # R_isPrimes_Function
+Function that detect primes Number, Fibonacci and such staff
---
+++ R_isPrimes_Function.R
@@ -0,0 +1,8 @@
+  isPrimes <- function(n) {
+      if ( (n %% 3) == 0 || (n %% 5) == 0  || (n %% 7) == 0 ) {
+          print ("is not prime")
+      } else {
+          print ("is prime")
+      }
+
+  }
